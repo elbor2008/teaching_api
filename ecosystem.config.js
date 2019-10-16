2fab23bbf572a075686f3418d9ecd2ab22a36183
Nick Nie <sky_elbor@hotmail.com>
2019-10-16 23:03:46 +1100

    modify pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'teaching api',
-      script: './build/index.js',
-      exp_backoff_restart_delay: 100,
-      instances: 0,
-      autorestart: true
+      script: './build/index.js'
     }
   ]
 };
